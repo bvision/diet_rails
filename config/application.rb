6aa3327738a5115c84710266e26c107efd2aaee1
Norman Clarke <norman@njclarke.com>
2012-03-08 12:41:27 -0300

    Set up basic prod/env environment differences

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,11 @@
 class MyApp < Rails::Application
   config.active_support.deprecation = :log
-  config.cache_classes = true
   config.middleware.delete "Rack::Lock"
   config.middleware.delete "ActionDispatch::Flash"
   config.middleware.delete "ActionDispatch::BestStandardsSupport"
-  config.secret_token = "13478cee671185055a01acc22c3960fe4d34300f"
+
+  config.secret_token                = "13478cee671185055a01acc22c3960fe4d34300f"
+  config.cache_classes               = Rails.env.production?
+  config.consider_all_requests_local = Rails.env.development?
+
 end
